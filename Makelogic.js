a22fbd839f011e339bcde15f59776c2858beb479
Hp <jabedhussainbarbhuiya99@gmail.com>
2022-02-28 22:40:19 -0800

    first commit

---
+++ Makelogic.js
@@ -0,0 +1,47 @@
+import React,{useState}  from 'react';
+import Data from "./Data";
+
+const App=()=>{
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+	
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < Data.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+	return (
+		<div className='app'>
+			{showScore ? (
+				<div className='score-section'>
+					You scored {score} out of {Data.length}
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{Data.length}
+						</div>
+						<div className='question-text'>{Data[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{Data[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+	);
+}
+export default App;
+
